3fc02afc739ebcc7a853f783e246c90d558c04f1
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2022-09-14 15:29:01 +0100

    [feat] Design a user avatar component to hold user image

---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    class="inline-block h-9 w-9 rounded-xl"
+    src="../assets/imgs/user-avatar.png"
+    alt="user avatar"
+  />
+</template>
+
+<script>
+export default {
+  name: "UserAvatar",
+};
+</script>
+
+<style lang="scss" scoped></style>
